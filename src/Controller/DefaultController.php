4d3d76475b495b233104152081281b21f638fddf
mtseluiko <mikhail.tseluiko@binary-studio.com>
2018-11-13 15:47:25 +0200

    Initial commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController
+{
+    public function index()
+    {
+        return new Response('Hello!');
+    }
+}
---
+++ src/Domain/Basket.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mikhail
+ * Date: 13.11.18
+ * Time: 15:45
+ */
+
+namespace App\Domain;
+
+
+class Basket
+{
+    private $id;
+    private $name;
+    private $maxCapacity;
+    private $contents;
+}
